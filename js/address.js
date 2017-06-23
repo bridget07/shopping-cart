05bf778a27ce0e5342fb9c13fe14a355d71b3ef8
高爽 <865967113@qq.com>
2017-06-23 15:36:43 +0800

    地址选配

--- js/address.js
+++ js/address.js
@@ -5,23 +5,37 @@ new Vue({
     el: ".container",
     data: {
         addressList: [],
-        limitNum: 3
+        limitNum: 3,
+        currentIndex: 0,
+        shippingMethod: 1
     },
     mounted: function () {
-        this.$nextTick(() => {
+        this.$nextTick(function() {
             this.getAddressList()
         })
     },
     computed: {
         filterAddress: function () {
-            return this.addressList.slice(0, limitNum)
+            return this.addressList.slice(0, this.limitNum)
         }
     },
     methods: {
         getAddressList: function () {
             this.$http.get("data/address.json").then(res => {
-                if(res.data.status === '0') {
+                // console.log("*****debug, res ", res)
+                // if(res.data.result.status === '0') {
+                //     console.log("*****debug, res.data ", res.data)
                     this.addressList = res.data.result
+                // }
+            })
+        },
+        setDefault: function(id) {
+            this.addressList .forEach((address, index) => {
+                if(address.addressId === id) {
+                    address.isDefault = true
+                }else {
+                    address.isDefault = false
+
                 }
             })
         }
